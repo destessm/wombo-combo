bd81b8d41c738f7fb5fb39297b99258b9b073d27
David <destessmargiassi@gmail.com>
2015-04-19 21:10:59 -0400

    Particles finished. New camera started.

---
+++ CS539Project/CS539Project/Controls.cpp
@@ -0,0 +1,9 @@
+//
+//  Controls.cpp
+//  CS539Project
+//
+//  Created by David Estes-Smargiassi on 4/19/15.
+//  Copyright (c) 2015 David Estes-Smargiassi. All rights reserved.
+//
+
+#include "Controls.h"
---
+++ CS539Project/CS539Project/Controls.h
@@ -0,0 +1,20 @@
+//
+//  Controls.h
+//  CS539Project
+//
+//  Created by David Estes-Smargiassi on 4/19/15.
+//  Copyright (c) 2015 David Estes-Smargiassi. All rights reserved.
+//
+
+#ifndef __CS539Project__Controls__
+#define __CS539Project__Controls__
+
+#include <stdio.h>
+#include "Angel.h"
+
+class Controls{
+    //unfinished
+};
+
+
+#endif /* defined(__CS539Project__Controls__) */
